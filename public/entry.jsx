860ba6173c80f7a0c298d292c118e4fa138dc935
Liuhaiyan6 <2391348695@qq.com>
2016-08-12 19:48:59 +0800

    recommendation

--- public/entry.jsx
+++ public/entry.jsx
@@ -1,6 +1,7 @@
 import MainBody from './components/edit.jsx';
 import NewsBody from './components/news.jsx'
 import Share from './components/share.jsx';
+import RemBody from './components/remmedation.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
@@ -9,6 +10,7 @@ require("bootstrap-webpack");
 require('./styles/edit.css');
 require('./styles/share.css');
 require('./styles/news.css');
+require('./styles/recommendation.css');
 
 
 ReactDOM.render((
@@ -16,6 +18,7 @@ ReactDOM.render((
     <Route path='/' component={MainBody}/>
     <Route path="/news" component={NewsBody}/>
     <Route path='/share' component={Share}/>
+    <Route path='/remmedation' component={RemBody}/>
   </Router>
 ), document.getElementById('mainbody'))
 
