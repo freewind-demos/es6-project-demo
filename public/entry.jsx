88084d2d3edb5222ae9b00415f07bd0d911a56f5
AmberYLopez <1163281758@qq.com>
2016-08-12 17:59:21 +0800

    index

--- public/entry.jsx
+++ public/entry.jsx
@@ -2,6 +2,7 @@ import MainBody from './components/edit.jsx';
 import NewsBody from './components/news.jsx'
 import Share from './components/share.jsx';
 import RemBody from './components/remmedation.jsx';
+import Index from './components/index.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
@@ -11,14 +12,16 @@ require('./styles/edit.css');
 require('./styles/share.css');
 require('./styles/news.css');
 require('./styles/recommendation.css');
-
+require('./styles/index.css')
 
 ReactDOM.render((
   <Router history={hashHistory}>
-    <Route path='/' component={MainBody}/>
-    <Route path="/news" component={NewsBody}/>
+    <Route path='/' component={Index}/>
+    <Route path='/news' component={NewsBody}/>
     <Route path='/share' component={Share}/>
+    <Route path='/edit' component={MainBody}/>
     <Route path='/remmedation' component={RemBody}/>
+    <Route path='/index' component={Index}/>
   </Router>
 ), document.getElementById('mainbody'))
 
